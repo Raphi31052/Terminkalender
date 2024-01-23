c776c9dab61e0ab90c0537ca4b26d951d1323e0b
Raphi <gruber.raphael.1@gmail.com>
2024-01-23 18:46:19 +0100

    Fixed reader

--- src/Terminkalender/Terminkalender.java
+++ src/Terminkalender/Terminkalender.java
@@ -33,20 +33,15 @@ public class Terminkalender {
 
 
         try {
-
             File myObj = new File("src/Terminkalender/meineTextdatei.txt");
             Scanner myReader = new Scanner(myObj);
-
-
-
-
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 System.out.println(data);
-
+            }
             myReader.close();
-        }}
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
